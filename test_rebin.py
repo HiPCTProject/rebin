fd36e39b01e8b850e81d7cd1e3d38f779de6d530
David Stansby <dstansby@gmail.com>
2023-07-17 18:47:15 +0100

    Add error test

--- test_rebin.py
+++ test_rebin.py
@@ -19,16 +19,22 @@ def populate_jp2_files(data: np.ndarray, path: Path) -> None:
 
 
 @pytest.mark.parametrize(
-    "array_in, expected_arrays", [(np.array([[[0, 1], [2, 5]]]), [np.array([[2]])])]
+    "array_in, bin_factor, expected_arrays",
+    [
+        (np.array([[[0, 1], [2, 5]]]), 2, [np.array([[2]])]),
+    ],
 )
 def test_rebin(
-    tmp_path: Path, array_in: np.ndarray, expected_arrays: list[np.ndarray]
+    tmp_path: Path,
+    array_in: np.ndarray,
+    bin_factor: int,
+    expected_arrays: list[np.ndarray],
 ) -> None:
     jp2_path = tmp_path / "input_jp2s"
     jp2_path.mkdir()
     populate_jp2_files(array_in, jp2_path)
 
-    output_dir = rebin(jp2_path, bin_factor=2)
+    output_dir = rebin(jp2_path, bin_factor=bin_factor)
     assert output_dir.exists()
     jp2_files = sorted(output_dir.glob("*.jp2"))
     assert len(jp2_files) == len(expected_arrays)
@@ -36,3 +42,24 @@ def test_rebin(
     for jp2_file, arr in zip(jp2_files, expected_arrays, strict=True):
         jp2 = glymur.Jp2k(str(jp2_file))
         np.testing.assert_equal(jp2[:], arr.astype(np.uint16))
+
+
+@pytest.mark.parametrize(
+    "array_in, bin_factor, err_msg",
+    [
+        # Bin factor = 1
+        (np.array([[[0, 1], [2, 5]]]), 1, "bin_factor must be > 1"),
+    ],
+)
+def test_rebin_errors(
+    tmp_path: Path,
+    array_in: np.ndarray,
+    bin_factor: int,
+    err_msg: str,
+) -> None:
+    jp2_path = tmp_path / "input_jp2s"
+    jp2_path.mkdir()
+    populate_jp2_files(array_in, jp2_path)
+
+    with pytest.raises(Exception, match=err_msg):
+        rebin(jp2_path, bin_factor=bin_factor)
